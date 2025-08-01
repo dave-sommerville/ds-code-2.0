635d187e837572a1cdc6f2d91ecf9d333dbf1dc2
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-08-01 12:01:12 -0500

    Maybe this time

--- src/Controls/UsePageTitle.js
+++ src/Controls/UsePageTitle.js
@@ -60,6 +60,18 @@ export default function UsePageTitle() {
       <title>{meta.title}</title>
       <meta name="description" content={meta.description} />
       <link rel="canonical" href="https://ds-code.ca/current-page-url" />
+      {/* Open Graph */}
+      <meta property="og:title" content={meta.title} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:image" content={meta.image} />
+      <meta property="og:url" content={fullUrl} />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter Card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:description" content={meta.description} />
+      <meta name="twitter:image" content={meta.image} />
     </Helmet>
   );
 }
