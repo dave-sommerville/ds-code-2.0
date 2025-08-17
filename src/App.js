fe71527f4ce4a3b970b9d89f08b53cf1d1ee99ba
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-08-16 21:16:27 -0500

    Added preload to app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import './css/roots.css';
 import './css/reset.css';
 import AppRoutes from './BLL/Routes';
 import ScrollToTop from './Controls/ScrollToTop';
+import {portfolioArray} from '../src/BLL/PortfolioItems';
+
 
 function App() {
+    {/* Preload all portfolio images */}
+  {portfolioArray.map((item, idx) => (
+    <img
+      key={`preload-${idx}`}
+      src={item.imgUrl}
+      alt={item.title}
+      style={{ display: 'none' }}
+      loading="eager"
+    />
+  ))}
   return(
     <>
       <ScrollToTop></ScrollToTop>
--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,6 +1,5 @@
 import HexLinks from "../Components/Portfolio/HexLinks";
 import SelectionInput from "../Components/Portfolio/SelectionInput";
-import {portfolioArray} from '../BLL/PortfolioItems';
 import { motion } from 'framer-motion';
 function Portfolio() {
   return(
@@ -11,16 +10,6 @@ function Portfolio() {
       transition={{ duration: 0.4 }}
     >
       <section className="portfolio-wrapper">
-        {/* Preload all portfolio images */}
-        {portfolioArray.map((item, idx) => (
-          <img
-            key={`preload-${idx}`}
-            src={item.imgUrl}
-            alt=""
-            style={{ display: 'none' }}
-            loading="eager"
-          />
-        ))}
         <SelectionInput />
         <HexLinks />
       </section>
