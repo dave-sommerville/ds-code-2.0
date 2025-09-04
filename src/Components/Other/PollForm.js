8524440c42a00fa1e2fb5238a4f6dd2ccea8e99a
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-09-04 11:46:23 -0500

    Updated Poll question

--- src/Components/Other/PollForm.js
+++ src/Components/Other/PollForm.js
@@ -1,34 +1,42 @@
 import '../../css/common/forms.css';
-function PollForm({classChoice}) {
+
+function PollForm({ classChoice }) {
   return (
-    <form name="poll" method="POST" data-netlify="true" netlify netlify-honeypot="bot-field" className={classChoice}>
-      <h5>What game should I create next?</h5>
+    <form
+      name="poll"
+      method="POST"
+      data-netlify="true"
+      netlify
+      netlify-honeypot="bot-field"
+      className={classChoice}
+    >
+      <h5>How do you feel about the growing role of AI in everyday life?</h5>
 
       {/* Hidden input for Netlify form handling */}
       <input type="hidden" name="form-name" value="poll" />
       <input type="hidden" name="bot-field" />
 
       <label className="poll-option pointer hover">
-        <input type="radio" name="feature" value="Cactus Jump" />
-        Cactus Jump
+        <input type="radio" name="ai-opinion" value="Excited" />
+        Excited — AI makes life easier and more innovative
       </label>
       <br />
 
       <label className="poll-option pointer hover">
-        <input type="radio" name="feature" value="Mine Sweeper" />
-        My Minesweeper
+        <input type="radio" name="ai-opinion" value="Cautious" />
+        Cautious — I see benefits, but also big risks
       </label>
       <br />
 
       <label className="poll-option pointer hover">
-        <input type="radio" name="feature" value="DS Paint" />
-        DS Paint
+        <input type="radio" name="ai-opinion" value="Neutral" />
+        Neutral — I’m not sure it affects me much
       </label>
       <br />
 
       <label className="poll-option pointer hover">
-        <input type="radio" name="feature" value="Whack a Hole" />
-        Whack a Hole
+        <input type="radio" name="ai-opinion" value="Skeptical" />
+        Skeptical — AI is overhyped and concerning
       </label>
       <br />
 
@@ -38,4 +46,5 @@ function PollForm({classChoice}) {
     </form>
   );
 }
+
 export default PollForm;
